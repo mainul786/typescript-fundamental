1847bc2b99eb83e5c78b5362890f359ebbf12a15
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-05-08 23:40:20 +0530

    class declaration

--- module-3/class.ts
+++ module-3/class.ts
@@ -1,14 +1,23 @@
 {
     // class declaretion
 
+    // class Animal{
+    //     name:string;
+    //     pecees:string;
+    //     sound:string;
+    //     constructor(name:string, pecees:string, sound:string){
+    //         this.name = name;
+    //         this.pecees = pecees;
+    //         this.sound = sound
+    //     }
+    //     makeSound(){
+    //         console.log(`The ${this.name} says ${this.sound}`)
+    //     }
+    // }
+// perfect process to declare class
     class Animal{
-        name:string;
-        pecees:string;
-        sound:string;
-        constructor(name:string, pecees:string, sound:string){
-            this.name = name;
-            this.pecees = pecees;
-            this.sound = sound
+       
+        constructor(public name:string, public pecees:string, public sound:string){
         }
         makeSound(){
             console.log(`The ${this.name} says ${this.sound}`)
@@ -16,5 +25,7 @@
     }
 
     const dog = new Animal('Germany Dog', 'Dog', 'ghew ghew');
-    dog.makeSound()
+    dog.makeSound();
+
+
 }
